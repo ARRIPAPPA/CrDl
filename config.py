e89a6c345d9ccbc0570e23ebc1105ad06c5ba477
ARRIPAPPA <137713659+ARRIPAPPA@users.noreply.github.com>
2024-03-09 14:25:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ class Config:
     # Telegram bot credentials
     TELEGRAM_API_ID = '16501053'
     TELEGRAM_API_HASH = 'd8c9b01c863dabacc484c2c06cdd0f6e'
-    TELEGRAM_BOT_TOKEN = '7060074172:AAHmrpyiVQuyNQKeg-R_x99uRcrHXEXFZRM'
+    TELEGRAM_BOT_TOKEN = '5932926601:AAHVnnl6kNepzUGkmxnMzMeu1P9jFPUsusk'
 
     # Crunchyroll credentials
     CR_EMAIL = 'dhanush6010@gmail.com'
